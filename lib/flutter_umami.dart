0f1af60f3751da6d1d3acb57fd704ce58bc6a0ba
Szymon Kowaliński <szymon@kowalinski.dev>
2025-06-14 14:54:38 +0200

    fix: export navigator observer

--- lib/flutter_umami.dart
+++ lib/flutter_umami.dart
@@ -1,5 +1,6 @@
-/// A Very Good Project created by Very Good CLI.
+/// Simple Umami Analytics Flutter SDK made by [Solvro](https://solvro.pwr.edu.pl).
 library;
 
 export "src/flutter_umami.dart";
+export "src/navigator_observer.dart";
 export "src/umami_collector.dart";
